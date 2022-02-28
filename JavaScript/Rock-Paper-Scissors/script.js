be89d1ec18f74f9498126720298327510417fb8c
Frédéric Ferreira <fred.69780@hotmail.fr>
2022-02-28 20:09:27 +0100

    JS interactif HTML

--- JavaScript/Rock-Paper-Scissors/script.js
+++ JavaScript/Rock-Paper-Scissors/script.js
@@ -1,9 +1,11 @@
-const btnPaper = document.querySelector(".paper");
-const imgPlayer = document.querySelector(".img-player");
-
-btnPaper.addEventListener("click", () => (imgPlayer.src = "hand.png"));
-
-/* Game Section */
+const btns = document.querySelectorAll("button");
+const restart = document.querySelector(".play-again");
+const pScore = document.querySelector(".player-score");
+const cScore = document.querySelector(".computer-score");
+const pChoice = document.querySelector(".player-choice");
+const cChoice = document.querySelector(".computer-choice");
+const results = document.querySelector(".results");
+const winner = document.querySelector(".winner");
 
 const rps = ["Rock", "Paper", "Scissors"];
 let randomIndex = Math.floor(Math.random() * rps.length);
@@ -11,10 +13,32 @@ let playerSelection;
 let computerSelection = computerPlay().toLowerCase();
 let playerScore = 0;
 let computerScore = 0;
-// game();
 
 function computerPlay() {
   return rps[randomIndex];
+}
+
+function playAgain() {
+  playerScore = 0;
+  computerScore = 0;
+  pScore.textContent = "0";
+  cScore.textContent = "0";
+  pChoice.classList.toggle("hide");
+  cChoice.classList.toggle("hide");
+  results.classList.toggle("hide");
+  btns.forEach((btn) => btn.classList.toggle("hide"));
+}
+
+function checkWinner() {
+  if (playerScore === 5 || computerScore === 5) {
+    pChoice.classList.add("hide");
+    cChoice.classList.add("hide");
+    results.classList.remove("hide");
+    winner.textContent = `${
+      playerScore > computerScore ? "You win" : "You lose"
+    }`;
+    btns.forEach((btn) => btn.classList.toggle("hide"));
+  } else return;
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -22,33 +46,41 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
-  )
+  ) {
     playerScore++;
-  else if (
+    pScore.textContent = `${playerScore}`;
+  } else if (
     (playerSelection === "rock" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "rock")
-  )
+  ) {
     computerScore++;
-  else return;
+    cScore.textContent = `${computerScore}`;
+  } else return;
 }
 
-function newComputerChoice() {
+const newComputerChoice = function () {
   randomIndex = Math.floor(Math.random() * rps.length);
   computerSelection = computerPlay().toLowerCase();
+};
+
+function game(choice) {
+  playerSelection = choice;
+  newComputerChoice();
+  playRound(playerSelection, computerSelection);
+  console.log(`player: ${playerScore} computer: ${computerScore}`);
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    playerSelection = prompt("Rock, Paper or Scissors ?").toLocaleLowerCase();
-    newComputerChoice();
-    playRound(playerSelection, computerSelection);
-    console.log(`player: ${playerScore} computer: ${computerScore}`);
-  }
+btns.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    const choice = e.target.getAttribute("class");
+    if (playerScore < 5 && computerScore < 5) {
+      game(choice);
+      checkWinner();
+    }
+  })
+);
 
-  playerScore === computerScore
-    ? console.log("No one wins🤷‍♂️")
-    : playerScore > computerScore
-    ? console.log("You win ! 😃")
-    : console.log("Computer wins ... 😔");
-}
+restart.addEventListener("click", playAgain);
+
+// game();
