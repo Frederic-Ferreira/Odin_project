c972e3f229dd03ca7614668006ec039c37f2c164
Frédéric Ferreira <fred.69780@hotmail.fr>
2022-03-01 09:49:31 +0100

    animations images ✅

--- JavaScript/Rock-Paper-Scissors/script.js
+++ JavaScript/Rock-Paper-Scissors/script.js
@@ -2,12 +2,14 @@ const btns = document.querySelectorAll("button");
 const restart = document.querySelector(".play-again");
 const pScore = document.querySelector(".player-score");
 const cScore = document.querySelector(".computer-score");
+const roundNumber = document.querySelector(".round");
 const pChoice = document.querySelector(".player-choice");
 const cChoice = document.querySelector(".computer-choice");
 const results = document.querySelector(".results");
 const winner = document.querySelector(".winner");
 const imgPlayer = document.querySelector(".img-player");
 const imgComputer = document.querySelector(".img-computer");
+const imgs = document.querySelectorAll(".img");
 
 const rps = ["Rock", "Paper", "Scissors"];
 let randomIndex = Math.floor(Math.random() * rps.length);
@@ -15,6 +17,7 @@ let playerSelection;
 let computerSelection = computerPlay().toLowerCase();
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
 
 function computerPlay() {
   return rps[randomIndex];
@@ -23,33 +26,43 @@ function computerPlay() {
 function playAgain() {
   playerScore = 0;
   computerScore = 0;
+  round = 0;
   pScore.textContent = "0";
   cScore.textContent = "0";
+  winner.textContent = "Le premier arrivé à 5 points à gagné !";
+  roundNumber.textContent = "Round 0";
   pChoice.classList.toggle("hide");
   cChoice.classList.toggle("hide");
-  results.classList.toggle("hide");
+  results.classList.remove("hide");
   imgPlayer.classList.toggle("hide");
   imgComputer.classList.toggle("hide");
   btns.forEach((btn) => btn.classList.toggle("hide"));
 }
 
 function displayImage(pChoice, cChoice) {
-  imgPlayer.classList.remove("hide");
-  imgComputer.classList.remove("hide");
+  if (!results.classList.contains("hide")) results.classList.add("hide");
+  if (
+    imgPlayer.classList.contains("hide") &&
+    imgPlayer.classList.contains("hide")
+  ) {
+    imgPlayer.classList.remove("hide");
+    imgComputer.classList.remove("hide");
+  }
+  imgComputer.classList.add("arrive");
+  imgPlayer.classList.add("arrive");
   imgPlayer.src = `${pChoice}.png`;
-  imgComputer.src = `${cChoice}.png`;
+  if (cChoice === "scissors") imgComputer.src = `${cChoice}2.png`;
+  else imgComputer.src = `${cChoice}.png`;
 }
 
 function checkWinner() {
-  if (playerScore === 5 || computerScore === 5) {
-    pChoice.classList.add("hide");
-    cChoice.classList.add("hide");
-    results.classList.remove("hide");
-    winner.textContent = `${
-      playerScore > computerScore ? "You win" : "You lose"
-    }`;
-    btns.forEach((btn) => btn.classList.toggle("hide"));
-  } else return;
+  pChoice.classList.add("hide");
+  cChoice.classList.add("hide");
+  results.classList.remove("hide");
+  winner.textContent = `${
+    playerScore > computerScore ? "You win" : "You lose"
+  }`;
+  btns.forEach((btn) => btn.classList.toggle("hide"));
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -59,6 +72,8 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
     playerScore++;
+    round++;
+    roundNumber.textContent = `Round ${round}`;
     pScore.textContent = `${playerScore}`;
   } else if (
     (playerSelection === "rock" && computerSelection === "paper") ||
@@ -67,7 +82,12 @@ function playRound(playerSelection, computerSelection) {
   ) {
     computerScore++;
     cScore.textContent = `${computerScore}`;
-  } else return;
+    round++;
+    roundNumber.textContent = `Round ${round}`;
+  } else {
+    round++;
+    roundNumber.textContent = `Round ${round}`;
+  }
 }
 
 const newComputerChoice = function () {
@@ -78,17 +98,25 @@ const newComputerChoice = function () {
 function game(choice) {
   playerSelection = choice;
   newComputerChoice();
-  playRound(playerSelection, computerSelection);
   displayImage(playerSelection, computerSelection);
+  setTimeout(function () {
+    imgPlayer.classList.remove("arrive");
+    imgComputer.classList.remove("arrive");
+    imgPlayer.classList.add("battle-player");
+    imgComputer.classList.add("battle-computer");
+  }, 2200);
+  setTimeout(() => {
+    playRound(playerSelection, computerSelection);
+    if (playerScore === 5 || computerScore === 5) checkWinner();
+    imgPlayer.classList.remove("battle-player");
+    imgComputer.classList.remove("battle-computer");
+  }, 2700);
 }
 
 btns.forEach((btn) =>
   btn.addEventListener("click", function (e) {
     const choice = e.target.getAttribute("class");
-    if (playerScore < 5 && computerScore < 5) {
-      game(choice);
-      checkWinner();
-    }
+    if (playerScore < 5 && computerScore < 5) game(choice);
   })
 );
 
