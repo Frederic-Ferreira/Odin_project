dbcb01e637c2d1e665228d6dad649426a51b0057
Frédéric Ferreira <fred.69780@hotmail.fr>
2022-03-10 10:37:54 +0100

    keyboard working but not DRY

--- JavaScript/calculator/script.js
+++ JavaScript/calculator/script.js
@@ -4,17 +4,94 @@ const numberBtns = document.querySelectorAll(".number");
 const operationBtns = document.querySelectorAll(".operator");
 const inputBtns = document.querySelectorAll(".input");
 
+// initialisation des valeurs pour savoir si on doit/peut remplacer et afficher le résultat
 let replace = true;
 let result = false;
 
+document.addEventListener("keydown", function (e) {
+  const k = e.key;
+  if (
+    k.includes("1") ||
+    k.includes("2") ||
+    k.includes("3") ||
+    k.includes("4") ||
+    k.includes("5") ||
+    k.includes("6") ||
+    k.includes("7") ||
+    k.includes("8") ||
+    k.includes("9") ||
+    k.includes("0") ||
+    k.includes(".")
+  ) {
+    if (k.includes(".")) if (inputWindow.textContent.includes(".")) return;
+    // Convertir en nombre que si ce n'est pas un point
+    const value = k === "." ? k : Number(k);
+    // si replace n'est pas initialiser ou que l'input est un point, ajouter valeur à la suite de l'input
+    replace === false || k === "."
+      ? (inputWindow.textContent += `${value}`)
+      : (inputWindow.textContent = value);
+    replace = false;
+  } else if (
+    k.includes("+") ||
+    k.includes("-") ||
+    k.includes("/") ||
+    k.includes("*") ||
+    k.includes("=")
+  ) {
+    const operator = k;
+    const previousOperator = operationsWindow.textContent.slice(-1);
+    const previousValue = Number(operationsWindow.textContent.slice(0, -2));
+    const currentValue = Number(inputWindow.textContent);
+    // remplacer l'input après une opération
+    replace = true;
+    if (!k.includes("=")) {
+      // si l'input des opérations est vide ou que un resultat a été effectué,
+      // remplacer l'input des opérations par le nouveau contenu, sinon effectuer
+      // l'opération
+      operationsWindow.textContent === "" || result === true
+        ? (operationsWindow.textContent =
+            inputWindow.textContent + ` ${operator}`)
+        : (operationsWindow.textContent =
+            isFloat(operate(previousOperator, previousValue, currentValue)) +
+            ` ${operator}`);
+      result = false;
+    } else {
+      // empêcher de voir un résultat sans opération et savoir si on doit remplacer
+      // l'input (si c'est 0) ou pas
+      if (operationsWindow.textContent === "") {
+        if (inputWindow.textContent == 0) return (replace = true);
+        else return (replace = false);
+      }
+      result = true;
+      operationsWindow.textContent =
+        operationsWindow.textContent + ` ${currentValue}`;
+      // afficher le résultat de l'opération dans la fenêtre input
+      inputWindow.textContent = isFloat(
+        operate(previousOperator, previousValue, currentValue)
+      );
+    }
+  } else if (k.includes("Backspace") || k.includes("Escape")) {
+    if (k.includes("Backspace"))
+      inputWindow.textContent = inputWindow.textContent.slice(0, -1);
+    else {
+      inputWindow.textContent = 0;
+      operationsWindow.textContent = "";
+      replace = true;
+    }
+  }
+});
+
 numberBtns.forEach((btn) =>
   btn.addEventListener("click", function (e) {
+    // si il y a déjà un point dans les chiffres, ne pas pouvoir en rajouter
     if (btn.classList.contains("dot"))
       if (inputWindow.textContent.includes(".")) return;
+    // Convertir en nombre que si ce n'est pas un point
     const value =
       e.target.textContent === "."
         ? e.target.textContent
         : Number(e.target.textContent);
+    // si replace n'est pas initialiser ou que l'input est un point, ajouter valeur à la suite de l'input
     replace === false || e.target.textContent === "."
       ? (inputWindow.textContent += `${value}`)
       : (inputWindow.textContent = value);
@@ -28,8 +105,12 @@ operationBtns.forEach((btn) =>
     const previousOperator = operationsWindow.textContent.slice(-1);
     const previousValue = Number(operationsWindow.textContent.slice(0, -2));
     const currentValue = Number(inputWindow.textContent);
+    // remplacer l'input après une opération
     replace = true;
     if (!e.target.classList.contains("equal")) {
+      // si l'input des opérations est vide ou que un resultat a été effectué,
+      // remplacer l'input des opérations par le nouveau contenu, sinon effectuer
+      // l'opération
       operationsWindow.textContent === "" || result === true
         ? (operationsWindow.textContent =
             inputWindow.textContent + ` ${operator}`)
@@ -38,10 +119,16 @@ operationBtns.forEach((btn) =>
             ` ${operator}`);
       result = false;
     } else {
-      if (operationsWindow.textContent === "") return;
+      // empêcher de voir un résultat sans opération et savoir si on doit remplacer
+      // l'input (si c'est 0) ou pas
+      if (operationsWindow.textContent === "") {
+        if (inputWindow.textContent == 0) return (replace = true);
+        else return (replace = false);
+      }
       result = true;
       operationsWindow.textContent =
         operationsWindow.textContent + ` ${currentValue}`;
+      // afficher le résultat de l'opération dans la fenêtre input
       inputWindow.textContent = isFloat(
         operate(previousOperator, previousValue, currentValue)
       );
@@ -51,6 +138,7 @@ operationBtns.forEach((btn) =>
 
 inputBtns.forEach((btn) =>
   btn.addEventListener("click", function (e) {
+    // boutons effacer ou remettre à 0
     if (e.target.classList.contains("delete"))
       inputWindow.textContent = inputWindow.textContent.slice(0, -1);
     else {
