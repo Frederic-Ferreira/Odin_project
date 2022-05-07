8e2ef79b9facac0dde340d604b9f0dc191bb7a52
Frédéric Ferreira <fred.69780@hotmail.fr>
2022-05-07 11:23:20 +0200

    JS check winner functionality ✅

--- JavaScript2.0/tic-tac-toe/dist/script.js
+++ JavaScript2.0/tic-tac-toe/dist/script.js
@@ -9,91 +9,7 @@ const gameBoard = {
   'third-row': ['', '', ''],
 };
 
-const winning1 =
-  gameBoard['first-row'][0] === gameBoard['first-row'][1] &&
-  gameBoard['first-row'][1] === gameBoard['first-row'][2] &&
-  gameBoard['first-row'][0] === 'x';
-const winning2 =
-  gameBoard['first-row'][0] === gameBoard['first-row'][1] &&
-  gameBoard['first-row'][1] === gameBoard['first-row'][2] &&
-  gameBoard['first-row'][0] === 'o';
-const winning3 =
-  gameBoard['first-row'][0] === gameBoard['second-row'][1] &&
-  gameBoard['second-row'][1] === gameBoard['third-row'][2] &&
-  gameBoard['first-row'][0] === 'x';
-const winning4 =
-  gameBoard['first-row'][0] === gameBoard['second-row'][1] &&
-  gameBoard['second-row'][1] === gameBoard['third-row'][2] &&
-  gameBoard['first-row'][0] === 'o';
-const winning5 =
-  gameBoard['first-row'][0] === gameBoard['second-row'][0] &&
-  gameBoard['first-row'][0] === gameBoard['second-row'][0] &&
-  gameBoard['first-row'][0] === 'x';
-const winning6 =
-  gameBoard['first-row'][0] === gameBoard['second-row'][0] &&
-  gameBoard['first-row'][0] === gameBoard['second-row'][0] &&
-  gameBoard['first-row'][0] === 'o';
-const winning7 =
-  gameBoard['second-row'][0] === gameBoard['second-row'][1] &&
-  gameBoard['second-row'][1] === gameBoard['second-row'][2] &&
-  gameBoard['second-row'][0] === 'x';
-const winning8 =
-  gameBoard['second-row'][0] === gameBoard['second-row'][1] &&
-  gameBoard['second-row'][1] === gameBoard['second-row'][2] &&
-  gameBoard['second-row'][0] === 'o';
-const winning9 =
-  gameBoard['third-row'][0] === gameBoard['second-row'][1] &&
-  gameBoard['second-row'][1] === gameBoard['first-row'][2] &&
-  gameBoard['third-row'][0] === 'x';
-const winning10 =
-  gameBoard['third-row'][0] === gameBoard['second-row'][1] &&
-  gameBoard['second-row'][1] === gameBoard['first-row'][2] &&
-  gameBoard['third-row'][0] === 'o';
-const winning11 =
-  gameBoard['third-row'][0] === gameBoard['third-row'][1] &&
-  gameBoard['third-row'][1] === gameBoard['third-row'][2] &&
-  gameBoard['third-row'][0] === 'x';
-const winning12 =
-  gameBoard['third-row'][0] === gameBoard['third-row'][1] &&
-  gameBoard['third-row'][1] === gameBoard['third-row'][2] &&
-  gameBoard['third-row'][0] === 'o';
-const winning13 =
-  gameBoard['third-row'][1] === gameBoard['second-row'][1] &&
-  gameBoard['second-row'][1] === gameBoard['first-row'][1] &&
-  gameBoard['third-row'][1] === 'x';
-const winning14 =
-  gameBoard['third-row'][1] === gameBoard['second-row'][1] &&
-  gameBoard['second-row'][1] === gameBoard['first-row'][1] &&
-  gameBoard['third-row'][1] === 'o';
-const winning15 =
-  gameBoard['third-row'][2] === gameBoard['second-row'][2] &&
-  gameBoard['second-row'][2] === gameBoard['first-row'][2] &&
-  gameBoard['third-row'][2] === 'x';
-const winning16 =
-  gameBoard['third-row'][2] === gameBoard['second-row'][2] &&
-  gameBoard['second-row'][2] === gameBoard['first-row'][2] &&
-  gameBoard['third-row'][2] === 'o';
-
-const winningCombinations = [
-  winning1,
-  winning2,
-  winning3,
-  winning4,
-  winning5,
-  winning6,
-  winning7,
-  winning8,
-  winning9,
-  winning10,
-  winning11,
-  winning12,
-  winning13,
-  winning14,
-  winning15,
-  winning16,
-];
-
-let activePlayer, playerOne, playerTwo;
+let activePlayer, playerOne, playerTwo, winner;
 
 const playerProto = (choice) => {
   return {
@@ -111,55 +27,101 @@ const choosePlayer = (choice) => {
   playGame();
 };
 
-const checkWinner = () => {
-  let winner;
+const checkCombinations = () => {
+  const winningCombinations = [
+    gameBoard['first-row'][0] === gameBoard['first-row'][1] &&
+      gameBoard['first-row'][1] === gameBoard['first-row'][2] &&
+      gameBoard['first-row'][0] === 'X',
+    gameBoard['first-row'][0] === gameBoard['first-row'][1] &&
+      gameBoard['first-row'][1] === gameBoard['first-row'][2] &&
+      gameBoard['first-row'][0] === 'O',
+    gameBoard['first-row'][0] === gameBoard['second-row'][1] &&
+      gameBoard['second-row'][1] === gameBoard['third-row'][2] &&
+      gameBoard['first-row'][0] === 'X',
+    gameBoard['first-row'][0] === gameBoard['second-row'][1] &&
+      gameBoard['second-row'][1] === gameBoard['third-row'][2] &&
+      gameBoard['first-row'][0] === 'O',
+    gameBoard['first-row'][0] === gameBoard['second-row'][0] &&
+      gameBoard['first-row'][0] === gameBoard['second-row'][0] &&
+      gameBoard['first-row'][0] === 'X',
+    gameBoard['first-row'][0] === gameBoard['second-row'][0] &&
+      gameBoard['first-row'][0] === gameBoard['second-row'][0] &&
+      gameBoard['first-row'][0] === 'O',
+    gameBoard['second-row'][0] === gameBoard['second-row'][1] &&
+      gameBoard['second-row'][1] === gameBoard['second-row'][2] &&
+      gameBoard['second-row'][0] === 'X',
+    gameBoard['second-row'][0] === gameBoard['second-row'][1] &&
+      gameBoard['second-row'][1] === gameBoard['second-row'][2] &&
+      gameBoard['second-row'][0] === 'O',
+    gameBoard['third-row'][0] === gameBoard['second-row'][1] &&
+      gameBoard['second-row'][1] === gameBoard['first-row'][2] &&
+      gameBoard['third-row'][0] === 'X',
+    gameBoard['third-row'][0] === gameBoard['second-row'][1] &&
+      gameBoard['second-row'][1] === gameBoard['first-row'][2] &&
+      gameBoard['third-row'][0] === 'O',
+    gameBoard['third-row'][0] === gameBoard['third-row'][1] &&
+      gameBoard['third-row'][1] === gameBoard['third-row'][2] &&
+      gameBoard['third-row'][0] === 'X',
+    gameBoard['third-row'][0] === gameBoard['third-row'][1] &&
+      gameBoard['third-row'][1] === gameBoard['third-row'][2] &&
+      gameBoard['third-row'][0] === 'O',
+    gameBoard['third-row'][1] === gameBoard['second-row'][1] &&
+      gameBoard['second-row'][1] === gameBoard['first-row'][1] &&
+      gameBoard['third-row'][1] === 'X',
+    gameBoard['third-row'][1] === gameBoard['second-row'][1] &&
+      gameBoard['second-row'][1] === gameBoard['first-row'][1] &&
+      gameBoard['third-row'][1] === 'O',
+    gameBoard['third-row'][2] === gameBoard['second-row'][2] &&
+      gameBoard['second-row'][2] === gameBoard['first-row'][2] &&
+      gameBoard['third-row'][2] === 'X',
+    gameBoard['third-row'][2] === gameBoard['second-row'][2] &&
+      gameBoard['second-row'][2] === gameBoard['first-row'][2] &&
+      gameBoard['third-row'][2] === 'O',
+  ];
 
   winningCombinations.forEach((combination, i) => {
-    if (combination) return (winner = i % 2 === 0 ? 'x' : 'o');
+    if (combination) winner = i % 2 === 0 ? 'x' : 'o';
   });
 
   return winner;
 };
 
 const playGame = () => {
-  gridEventListener();
-  checkWinner();
+  checkCombinations();
   if (winner !== undefined) console.log('The winner is ' + winner);
   if (
-    gameBoard['first-row'].length === 2 &&
-    gameBoard['second-row'].length === 2 &&
-    gameBoard['third-row'].length === 2
+    gameBoard['first-row'].length === 3 &&
+    gameBoard['second-row'].length === 3 &&
+    gameBoard['third-row'].length === 3
   )
     console.log('Its a tie');
 };
 
-function gridEventListener() {
-  gridCells.forEach((cell) => {
-    cell.addEventListener('click', (e) => {
-      const i = e.target.dataset.cell;
-      const fill = activePlayer.choice;
+gridCells.forEach((cell) => {
+  cell.addEventListener('click', (e) => {
+    const i = e.target.dataset.cell;
+    const fill = activePlayer.choice;
 
-      if (i <= 3) {
-        if (gameBoard['first-row'][i - 1] === '') {
-          gameBoard['first-row'][i - 1] = fill;
-          cell.textContent = fill;
-        }
-      } else if (i <= 6) {
-        if (gameBoard['second-row'][i - 4] === '') {
-          gameBoard['second-row'][i - 4] = fill;
-          cell.textContent = fill;
-        }
-      } else {
-        if (gameBoard['third-row'][i - 7] === '') {
-          gameBoard['third-row'][i - 7] = fill;
-          cell.textContent = fill;
-        }
+    if (i <= 3) {
+      if (gameBoard['first-row'][i - 1] === '') {
+        gameBoard['first-row'][i - 1] = fill;
+        cell.textContent = fill;
       }
-      activePlayer =
-        activePlayer === playerOne ? playerTwo : playerOne;
-    });
+    } else if (i <= 6) {
+      if (gameBoard['second-row'][i - 4] === '') {
+        gameBoard['second-row'][i - 4] = fill;
+        cell.textContent = fill;
+      }
+    } else {
+      if (gameBoard['third-row'][i - 7] === '') {
+        gameBoard['third-row'][i - 7] = fill;
+        cell.textContent = fill;
+      }
+    }
+    activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+    playGame();
   });
-}
+});
 
 choices.forEach((choice) => {
   choice.addEventListener('click', (e) => {
