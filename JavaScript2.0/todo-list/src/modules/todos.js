73f73a54952f0f868e08dc6a70154b9bbf6f6e9a
Frédéric Ferreira <fred.69780@hotmail.fr>
2022-05-12 21:53:03 +0200

    great improvements! 👏

--- JavaScript2.0/todo-list/src/modules/todos.js
+++ JavaScript2.0/todo-list/src/modules/todos.js
@@ -11,8 +11,45 @@ export default class todos {
     };
   }
 
-  static modifyTodo(todoNode) {
-    console.log(todoNode);
+  static modifyTodoForm(todoNode) {
+    const title = todoNode.childNodes[3].textContent;
+    const date = todoNode.childNodes[5].textContent;
+    const priority = todoNode.classList.value.slice(5);
+
+    const li = document.createElement('li');
+    const html = `
+    <form class="modified-todo-form">
+      <label for="title">Title</label>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        minlength="3"
+        value="${title}"
+        required
+      />
+      <label for="date">Date</label>
+      <input type="date" id="date" name="date" value="${this.inputFormattedDate(
+        date
+      )}" required />
+      <label for="priority">Priority</label>
+      <select id="priority" name="priority">
+        <option value="high" ${
+          priority === 'high' ? 'selected' : ''
+        }>High</option>
+        <option value="normal" ${
+          priority === 'normal' ? 'selected' : ''
+        }>Normal</option>
+        <option value="low" ${
+          priority === 'low' ? 'selected' : ''
+        }>Low</option>
+      </select>
+      <button>OK</button>
+      <span class="exit--new-todo">&times;</span>
+  </form>`;
+    li.classList.add('new-todo');
+    li.insertAdjacentHTML('afterbegin', html);
+    todoNode.closest('#todo-list').replaceChild(li, todoNode);
   }
 
   static getTitle(todo) {
@@ -24,6 +61,13 @@ export default class todos {
     const month = todoDate.split('-')[1];
     const day = todoDate.split('-')[2];
     return `${day}/${month}/${year}`;
+  }
+
+  static inputFormattedDate(todoDate) {
+    const year = todoDate.split('/')[2];
+    const month = todoDate.split('/')[1];
+    const day = todoDate.split('/')[0];
+    return `${year}-${month}-${day}`;
   }
 
   static getPriority = (todo) => {
--- JavaScript2.0/todo-list/src/modules/ui.js
+++ JavaScript2.0/todo-list/src/modules/ui.js
@@ -9,10 +9,11 @@ const date = document.getElementById('date');
 const priority = document.getElementById('priority');
 
 const btnAddTodo = document.getElementById('create-todo');
-const btnsEditTodo = document.querySelectorAll('.edit');
 const btnExitNewTodo = document.querySelector('.exit--new-todo');
 
 export default class UI {
+  edition = false;
+
   static todoFormEventListener() {
     todoForm.addEventListener('submit', (e) => {
       e.preventDefault();
@@ -36,11 +37,42 @@ export default class UI {
   }
 
   static editTodoEventListener() {
-    btnsEditTodo.forEach((btn) => {
-      btn.addEventListener('click', (e) => {
-        todos.modifyTodo(e.target);
-      });
+    document.addEventListener('click', (e) => {
+      if (!e.target.classList.contains('edit')) return;
+
+      todos.modifyTodoForm(e.target.closest('li'));
+      UI.modifiedTodoFormEventListener();
     });
+  }
+
+  static modifiedTodoFormEventListener() {
+    const form = document.querySelector('.modified-todo-form');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const form = e.target.closest('form');
+      UI.renderModifiedTodo(form);
+    });
+  }
+
+  static renderModifiedTodo(form) {
+    const title = form.childNodes[3].value;
+    const date = form.childNodes[7].value;
+    const priority = form.childNodes[11].value;
+
+    const li = document.createElement('li');
+    const html = `
+          <div class="check"></div>
+            <h3>${title}</h3>
+            <p>${todos.getFormattedDate(date)}</p>
+            <div class="span-todo-list">
+              <i class="edit outline icon"></i>
+              <i class="trash alternate outline icon"></i>
+          </div>
+    `;
+    li.classList.add('todo');
+    li.classList.add(priority);
+    li.insertAdjacentHTML('afterbegin', html);
+    form.closest('#todo-list').replaceChild(li, form.closest('li'));
   }
 
   static addTodoEventListener() {
