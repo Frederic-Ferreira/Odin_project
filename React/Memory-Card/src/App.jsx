07c55fec40631470ba65228eafc618e3dcbc33cf
Frédéric Ferreira <fred.69780@hotmail.fr>
2022-06-03 18:45:18 +0200

    GAME FINISHED! 👍 👏 🏁

--- React/Memory-Card/src/App.jsx
+++ React/Memory-Card/src/App.jsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import Video from './components/Video';
+import Menu from './components/Menu';
+import Winning from './components/Winning';
 import Cardboard from './components/Carboard';
 
 function App() {
   const [score, setScore] = useState({ current: 10, best: 0 });
+  const [showMenu, setShowMenu] = useState(true);
   const [winner, setWinner] = useState(false);
+
+  const init = () => {
+    setScore({
+      current: 0,
+      best: score.best,
+    });
+    setShowMenu(false);
+    setWinner(false);
+  };
 
   const increment = () => {
     setScore({
@@ -21,27 +32,39 @@ function App() {
   };
 
   useEffect(() => {
-    if (score.current === 12) setWinner(true);
+    if (score.current === 12) {
+      setWinner(true);
+      setScore({
+        current: score.current,
+        best: score.current,
+      });
+    }
   }, [score.current]);
 
   return (
     <div className="App">
       <header>
         <h1>Memory Card</h1>
-        <h2 id="display">Never click on the same card twice!</h2>
+        {showMenu ? (
+          ''
+        ) : (
+          <h2 id="display">Never click on the same card twice!</h2>
+        )}
         <div className="scores">
           <div className="inline-score">
             <h3>Current score: </h3>
-            <p>{score.current}</p>
+            {showMenu ? '' : <p>{score.current}</p>}
           </div>
           <div className="inline-score">
             <h3>Best score: </h3>
-            <p>{score.best}</p>
+            {showMenu ? '' : <p>{score.best}</p>}
           </div>
         </div>
       </header>
-      {winner ? (
-        <Video />
+      {showMenu ? (
+        <Menu play={init} />
+      ) : winner ? (
+        <Winning replay={init} />
       ) : (
         <Cardboard
           increment={increment}
